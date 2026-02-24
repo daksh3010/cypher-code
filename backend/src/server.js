5aaa2a05725b29fc13f3c32580a197ecf8890f18
Daksh <dakshkundu30102002@gmail.com>
2026-02-24 23:06:55 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { ENV } from './lib/env.js';
+
+const app = express();
+console.log(ENV.PORT);
+app.get("/" , (req,res) => {
+    res.status(200).json({msg : "api is running"});
+
+}) ;
+
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+}) ;
